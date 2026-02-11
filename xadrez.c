f71c10d259dff200e15207c020f2a8ac63adaeb0
Aquiles <105252462+AquilesFullStack@users.noreply.github.com>
2026-02-10 23:33:22 -0300

    Implement basic movement for chess pieces

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,32 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    //movimentações simples de peças de xadrez
+   int torre = 5;
+   int bispo;
+   int rainha;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+   printf("MOVIMENTAÇÃO DA TORRE\n");
+   for (int i = 0; i <= torre; i++)
+   {
+    printf("Movimentando torre para direita - casa %d \n", i);
+   }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+   printf("MOVIMENTAÇÃO DO BISPO\n");
+   int i = 0;   
+   while (i <= 5 )
+   {
+    printf("Movimento bispo para CIMA, DIREITA na casa %d \n", i );
+    i++;
+   }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+   printf("MOVIMENTAÇÃO DA RAINHA\n");
+   int j = 0;
+   do {
+    printf("Movimento da rainha para a esquerda na casa %d \n", j);
+    j++;
+   }
+   while(j <= 8);
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+   return 0;
 }
